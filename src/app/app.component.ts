167ec12dbbf1f57bf017f33aebe7733f8816ed12
nafseerck7 <nafseer.newsreels@gmail.com>
2022-07-12 17:39:34 +0400

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { APIService } from 'src/services/API/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'admin-dashboard';
+  constructor(private apiService : APIService){
+
+  }
+  ngOnInit(){
+this.apiService.getData().subscribe((res) => {
+  console.log("response", res)
+},
+(error)=> {
+  console.log("error", error)
+})
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpAuthInterceptor } from 'src/services/auth.interceptors';
 
 import { AppComponent } from './app.component';
-import { SourcesComponent } from './sources/sources.component';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SourcesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: HttpAuthInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
