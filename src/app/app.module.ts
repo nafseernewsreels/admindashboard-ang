b95271b646ba0ea156e15e69833047cc8adc92da
nafseerck7 <nafseer.newsreels@gmail.com>
2022-07-12 11:26:48 +0400

    ui changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SourcesComponent } from './sources/sources.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SourcesComponent
   ],
   imports: [
     BrowserModule
